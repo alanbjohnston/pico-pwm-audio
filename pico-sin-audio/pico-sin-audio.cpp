4dde89d3036b134102786c3cec62778c6caa0cf5
alanbjohnston <alan.b.johnston@gmail.com>
2022-08-27 11:35:40 -0400

    Update pico-sin-audio.cpp

--- pico-sin-audio/pico-sin-audio.cpp
+++ pico-sin-audio/pico-sin-audio.cpp
@@ -9,12 +9,15 @@
  * for converting audio samples into static arrays. 
  */
 //#include "../sample.h"
-int wav_position = 0;
-int counter = 0;
-int counter_max = 25;  // 1.85kHz
-int amplitude = 120;
-int value = 128 - amplitude;
-int rnd_bit = 1;
+int pwm_wav_position = 0;
+int pwm_counter = 0;
+int pwm_counter_max = 25;  // 1.85kHz
+int pwm_amplitude = 120;
+int pwm_value = 128 - pwm_amplitude;
+int pwm_rnd_bit = 1;
+int pwm_audio_pin = 26;
+bool pwm_audio_on = false;
+
 /*
  * PWM Interrupt Handler which outputs PWM level and advances the 
  * current sample. 
@@ -23,98 +26,38 @@ int rnd_bit = 1;
  * adjust by factor of 8   (this is what bitshifting <<3 is doing)
  * 
  */
+
+void pwm_sin_set_pin(int pin) {
  
-/* 
-void pwm_interrupt_handler() {
-    pwm_clear_irq(pwm_gpio_to_slice_num(AUDIO_PIN));    
-    if (wav_position < (WAV_DATA_LENGTH<<3) - 1) { 
-        // set pwm level 
-        // allow the pwm value to repeat for 8 cycles this is >>3 
-        pwm_set_gpio_level(AUDIO_PIN, WAV_DATA[wav_position>>3]);  
-        wav_position++;
-    } else {
-        // reset to start
-        wav_position = 0;
-    }
-}
-*/
-
-void pwm_interrupt_handler2() {
-    pwm_clear_irq(pwm_gpio_to_slice_num(AUDIO_PIN)); 
-    counter++;   
- //   if (wav_position < (WAV_DATA_LENGTH<<3) - 1) { 
-        // set pwm level 
-        // allow the pwm value to repeat for 8 cycles this is >>3 
-
-
-        if (counter > 450) {
-          counter -= 450;
-          if (random(0,2) == 1)
-            rnd_bit *= (-1.0);
-        
-          if ((value == (128 - amplitude)) && (rnd_bit == 1)) {
-            value = 128 + amplitude;
-            Serial.println("High");
-          }
-          else {
-            value = 128 - amplitude; 
-            Serial.println("Low");
-          }
-        }  
-        pwm_set_gpio_level(AUDIO_PIN, value);  
-        wav_position++;
-//    } else {
-        // reset to start
-//        wav_position = 0;
-//    }
+  pwm_audio_pin = pin;
+ 
 }
 
 void pwm_interrupt_handler3() {
-    pwm_clear_irq(pwm_gpio_to_slice_num(AUDIO_PIN)); 
-    counter++;   
-    if (counter > counter_max) {
-      counter -= counter_max;
+  pwm_clear_irq(pwm_gpio_to_slice_num(pwm_audio_pin)); 
+  if (pwm_audio_on) {
+    pwm_counter++;   
+    if (pwm_counter > pwm_counter_max) {
+      pwm_counter -= pwm_counter_max;
     }
 
-    value = int (128 + amplitude * sin(counter * 6.28 / (float)(counter_max)));
+    pwm_value = int (128 + pwm_amplitude * sin(pwm_counter * 6.28 / (float)(pwm_counter_max)));
  //   Serial.println(value);
 
-    pwm_set_gpio_level(AUDIO_PIN, value);  
+    pwm_set_gpio_level(pwm_audio_pin, pwm_value);  
+  }
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
- pinMode(17, OUTPUT);
-  digitalWrite(17, LOW);  // start transmitting
-
-  pinMode(22, OUTPUT);
-  digitalWrite(22, HIGH);
-
-  pinMode(28, INPUT);
-  pinMode(27, INPUT);
-  
-  Serial1.begin(9600);
-  delay(2000);
-  Serial.println("Starting up!");
-  
-  delay(500);
-//  Serial1.println("AT+DMOSETGROUP=0,434.9000,434.9000,1,2,1,1\r");
-  Serial1.println("AT+DMOSETGROUP=0,434.9000,434.9000,1,2,1,1\r");
-  delay(500);
-  
-}
-
-void loop() {
+void pwm_sin_start() {
     /* Overclocking for fun but then also so the system clock is a 
      * multiple of typical audio sampling rates.
      */
 //    stdio_init_all();
 //    set_sys_clock_khz(176000, true); 
-    set_sys_clock_khz(125000, true); 
-    gpio_set_function(AUDIO_PIN, GPIO_FUNC_PWM);
+//    set_sys_clock_khz(125000, true); 
+    gpio_set_function(pwm_audio_pin, GPIO_FUNC_PWM);
 
-    int audio_pin_slice = pwm_gpio_to_slice_num(AUDIO_PIN);
+    int audio_pin_slice = pwm_gpio_to_slice_num(pwm_audio_pin);
 
     // Setup PWM interrupt to fire when PWM cycle is complete
     pwm_clear_irq(audio_pin_slice);
@@ -140,11 +83,13 @@ void loop() {
     pwm_config_set_wrap(&config, 250); 
     pwm_init(audio_pin_slice, &config, true);
 
-    pwm_set_gpio_level(AUDIO_PIN, 0);
+    pwm_set_gpio_level(pwm_audio_pin, 0);
+ 
+     pwm_audio_on = true;
+}
 
-    while(1) {
-        __wfi(); // Wait for Interrupt
-        delay(1000);
-        Serial.println("Working");
-    }
+void pwm_sin_stop() {
+ 
+    pwm_audio_on = false;
+ 
 }
