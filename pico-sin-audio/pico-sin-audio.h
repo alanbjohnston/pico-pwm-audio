b688dd45e7514c767ce408b1f54e309ce1403f6e
alanbjohnston <alan.b.johnston@gmail.com>
2022-08-27 11:24:16 -0400

    Create pico-sin-audio.h

---
+++ pico-sin-audio/pico-sin-audio.h
@@ -0,0 +1,12 @@
+#ifndef PICO_SIN_AUDIO_H
+#define PICO_SIN_AUDIO_H
+
+#include <Arduino.h>
+
+#include <stdio.h>
+#include "pico/stdlib.h"   // stdlib 
+#include "hardware/irq.h"  // interrupts
+#include "hardware/pwm.h"  // pwm 
+#include "hardware/sync.h" // wait for interrupt 
+
+#endif
